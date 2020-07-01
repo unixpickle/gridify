c8ea1b7dbd1a73c0a8a525b1a25d8711dd4fa31d
Alex Nichol <unixpickle@gmail.com>
2020-07-01 16:12:45 -0400

    add usage string, use auto-grid layout

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"image"
 	"image/jpeg"
 	"image/png"
-	"math"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -16,53 +15,41 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		DieUsage()
-	}
-	subCommand := os.Args[1]
-	switch subCommand {
-	case "make-grid":
-		essentials.Must(MakeGrid(os.Args[2:]))
-	default:
-		fmt.Fprintln(os.Stderr, "unknown sub-command: "+subCommand)
-		fmt.Fprintln(os.Stderr)
-		DieUsage()
-	}
-}
-
-func MakeGrid(args []string) error {
 	var cols int
-	fs := flag.NewFlagSet("make-grid", flag.ExitOnError)
-	fs.IntVar(&cols, "cols", 0, "number of columns (0 means use sqrt)")
-	fs.Usage = func() {
-		fmt.Fprintln(os.Stderr, "Usage: gridify make-grid [flags] <inputs ...> <output>")
+	flag.IntVar(&cols, "cols", 0, "number of columns (0 to layout automatically)")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: gridify [flags] <inputs ...> <output>")
 		fmt.Fprintln(os.Stderr)
-		fs.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "Input files may themselves be grids. To read sub-images from a grid,")
+		fmt.Fprintln(os.Stderr, "place an '@' at the end of the name, followed by the grid size:")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "  <path>@<rows>x<cols> (e.g. /my/image.png@2x4)")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "Available flags:")
+		fmt.Fprintln(os.Stderr)
+		flag.PrintDefaults()
 		fmt.Fprintln(os.Stderr)
 		os.Exit(1)
 	}
-	fs.Parse(args)
-	if len(fs.Args()) < 2 {
-		fs.Usage()
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		flag.Usage()
 	}
 
 	var tiles []image.Image
-	for _, imageName := range fs.Args()[:len(fs.Args())-1] {
+	for _, imageName := range flag.Args()[:len(flag.Args())-1] {
 		images, err := ImagesFromFilename(imageName)
 		if err != nil {
-			return fmt.Errorf("read %s: %s", imageName, err.Error())
+			essentials.Die(fmt.Sprintf("read %s: %s", imageName, err.Error()))
 		}
 		tiles = append(tiles, images...)
 	}
 	if cols == 0 {
-		cols = int(math.Ceil(math.Sqrt(float64(len(tiles)))))
-		if cols == 0 {
-			cols = 1
-		}
+		cols = AutoGridColumns(tiles)
 	}
 
 	output := PlaceInGrid(tiles, cols)
-	return WriteImageToFile(fs.Args()[len(fs.Args())-1], output)
+	essentials.Must(WriteImageToFile(flag.Args()[len(flag.Args())-1], output))
 }
 
 func ImagesFromFilename(name string) ([]image.Image, error) {
@@ -110,13 +97,4 @@ func WriteImageToFile(outName string, img image.Image) error {
 	} else {
 		return jpeg.Encode(w, img, nil)
 	}
-}
-
-func DieUsage() {
-	fmt.Fprintln(os.Stderr, "Usage: gridify sub_command args")
-	fmt.Fprintln(os.Stderr, "")
-	fmt.Fprintln(os.Stderr, "Available sub-commands:")
-	fmt.Fprintln(os.Stderr, "  make-grid [-cols N] <inputs ...> <output>")
-	fmt.Fprintln(os.Stderr, "")
-	os.Exit(1)
 }
