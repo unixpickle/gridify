3a91370240ef3fdbb095dfbebb589cc6911a5891
Alex Nichol <unixpickle@gmail.com>
2020-07-02 16:49:02 -0400

    split out image I/O, better errors, add GIF

---
+++ image_file.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/unixpickle/essentials"
+)
+
+// ReadImagesFromFile extracts one or more images from a
+// file, which may contain a grid of sub-images.
+//
+// If the filename contains an @, then it will be parsed as
+//
+//     <path>@<rows>x<cols>
+//
+// and the images from the grid will be returned in order
+// (according to scanline ordering).
+func ReadImagesFromFile(name string) (imgs []image.Image, err error) {
+	defer essentials.AddCtxTo(fmt.Sprintf("read images %s", name), &err)
+
+	name, rows, cols, err := parseGridFilename(name)
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	img, _, err := image.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	return ExtractFromGrid(img, rows, cols)
+}
+
+func parseGridFilename(name string) (base string, rows, cols int, err error) {
+	base = name
+	rows = 1
+	cols = 1
+
+	idx := strings.LastIndex(name, "@")
+	if idx != -1 {
+		base = name[:idx]
+		dimStr := name[idx+1:]
+		parts := strings.Split(dimStr, "x")
+
+		err = fmt.Errorf("invalid grid size after '@': expected <rows>x<cols> but got %s", dimStr)
+		if len(parts) != 2 {
+			return
+		}
+		var intErr error
+		rows, intErr = strconv.Atoi(parts[0])
+		if intErr != nil {
+			return
+		}
+		cols, err = strconv.Atoi(parts[1])
+		if intErr != nil {
+			return
+		}
+		err = nil
+	}
+
+	return
+}
+
+// WriteImageToFile saves an image to a file, using the
+// extension as an indicator of the file type.
+func WriteImageToFile(outName string, img image.Image) (err error) {
+	defer essentials.AddCtxTo(fmt.Sprintf("write image %s", outName), &err)
+	w, err := os.Create(outName)
+	if err != nil {
+		return err
+	}
+	defer w.Close()
+
+	ext := strings.ToLower(filepath.Ext(outName))
+	switch ext {
+	case ".png":
+		return png.Encode(w, img)
+	case ".jpg", ".jpeg":
+		return jpeg.Encode(w, img, nil)
+	case ".gif":
+		return gif.Encode(w, img, nil)
+	default:
+		return fmt.Errorf("unknown file extension: %s", ext)
+	}
+}
--- main.go
+++ main.go
@@ -4,12 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"image"
-	"image/jpeg"
-	"image/png"
 	"os"
-	"path/filepath"
-	"strconv"
-	"strings"
 
 	"github.com/unixpickle/essentials"
 )
@@ -38,10 +33,8 @@ func main() {
 
 	var tiles []image.Image
 	for _, imageName := range flag.Args()[:len(flag.Args())-1] {
-		images, err := ImagesFromFilename(imageName)
-		if err != nil {
-			essentials.Die(fmt.Sprintf("read %s: %s", imageName, err.Error()))
-		}
+		images, err := ReadImagesFromFile(imageName)
+		essentials.Must(err)
 		tiles = append(tiles, images...)
 	}
 	if cols == 0 {
@@ -50,51 +43,4 @@ func main() {
 
 	output := PlaceInGrid(tiles, cols)
 	essentials.Must(WriteImageToFile(flag.Args()[len(flag.Args())-1], output))
-}
-
-func ImagesFromFilename(name string) ([]image.Image, error) {
-	idx := strings.LastIndex(name, "@")
-	rows := 1
-	cols := 1
-	if idx != -1 {
-		dimensions := name[idx+1:]
-		name = name[:idx]
-		parts := strings.Split(dimensions, "x")
-		partErr := fmt.Errorf("invalid grid size string after '@': %s (expected NxM)", dimensions)
-		if len(parts) != 2 {
-			return nil, partErr
-		}
-		var err error
-		rows, err = strconv.Atoi(parts[0])
-		if err != nil {
-			return nil, partErr
-		}
-		cols, err = strconv.Atoi(parts[1])
-		if err != nil {
-			return nil, partErr
-		}
-	}
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	img, _, err := image.Decode(f)
-	if err != nil {
-		return nil, err
-	}
-	return ExtractFromGrid(img, rows, cols)
-}
-
-func WriteImageToFile(outName string, img image.Image) error {
-	w, err := os.Create(outName)
-	if err != nil {
-		return err
-	}
-	defer w.Close()
-	if filepath.Ext(outName) == ".png" {
-		return png.Encode(w, img)
-	} else {
-		return jpeg.Encode(w, img, nil)
-	}
 }
